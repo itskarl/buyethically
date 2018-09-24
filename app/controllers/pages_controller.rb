0ceeb017bf0d01bcacd57901769b7cd0736d14f3
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-24 15:02:57 -0400

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
     if params[:upc_input]
-    searched = params[:upc_input]
-    @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{searched}")
-    @req = JSON.parse(@data.body_str)
+      searched = params[:upc_input]
+      @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{searched}")
+      @req = JSON.parse(@data.body_str)
     end
   end
 
