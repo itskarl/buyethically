e99348cb9b63097a6bd8d46c636b8f2b19754e1c
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-24 14:28:22 -0400

    heroku test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
+    if params[:upc_input]
+    searched = params[:upc_input]
+    @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{searched}")
+    @req = JSON.parse(@data.body_str)
+    end
   end
 
   def about
