5fab159959a53992f5101c73881676c2696c9f0d
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 00:58:57 -0400

    hope this works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,17 @@ class PagesController < ApplicationController
   def home
     if params[:upc_input]
       searched = params[:upc_input]
-      @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{searched}")
-      @req = JSON.parse(@data.body_str)
+      # @data = Curl::Easy.perform("http://api.walmartlabs.com/v1/items?apiKey=e7cvndf2d9pv8yv77cgnebcg&upc=#{searched}")
+      # @req = JSON.parse(@data.body_str)
+      @doc = Nokogiri::HTML(open("https://www.barcodelookup.com/#{searched}"))
+      @manufacturer = doc.search('p')[1].text
     end
+
+
+
+    rescue StandardError
+    rescue Exception
+
   end
 
   def about
