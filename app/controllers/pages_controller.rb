4246b810b6e51047c9fcc9c9b0b0f671d94a92c7
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 11:47:14 -0400

    help

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,13 @@ class PagesController < ApplicationController
   def home
     if params[:upc_input]
       searched = params[:upc_input]
-      # @data = Curl::Easy.perform("http://api.walmartlabs.com/v1/items?apiKey=e7cvndf2d9pv8yv77cgnebcg&upc=#{searched}")
-      # @req = JSON.parse(@data.body_str)
-
-      require 'open-uri'
-      @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/#{searched}"))
-      @manufacturer = @doc.search('p')[1].text
+      @data = Curl::Easy.perform("http://api.walmartlabs.com/v1/items?apiKey=e7cvndf2d9pv8yv77cgnebcg&upc=#{searched}")
+      @req = JSON.parse(@data.body_str)
+      @manufacturer = @req['item']['manufacturer']
+      # 
+      # require 'open-uri'
+      # @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/#{searched}"))
+      # @manufacturer = @doc.search('p')[1].text
     end
 
     rescue StandardError
