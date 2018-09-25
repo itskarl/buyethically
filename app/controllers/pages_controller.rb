34d3c1afddd4e33997d82750cbfe6ee2385d977a
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 12:20:00 -0400

    push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     if params[:upc_input]
       searched = params[:upc_input]
-      @data = Curl::Easy.perform("	https://www.digit-eyes.com/gtin/v2_0/?upcCode=#{searched}&field_names=all&language=en&app_key=/+90u/IFDVg6&signature=KN/BBmEUE4tKFPemTydKvRdmADY=")
+      @data = Curl::Easy.perform("https://www.digit-eyes.com/gtin/v2_0/?upcCode=#{searched}&field_names=all&language=en&app_key=/+90u/IFDVg6&signature=KN/BBmEUE4tKFPemTydKvRdmADY=")
       @req = JSON.parse(@data.body_str)
       # @manufacturer = @req['item']['manufacturer']
       @company = @req['manufacturer']['company']
