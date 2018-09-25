460a2a1b7ef8303a7c58b0deabcc27eb5440053d
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 12:34:08 -0400

    ah

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,16 +4,16 @@ class PagesController < ApplicationController
       searched = params[:upc_input]
       @data = Curl::Easy.perform("https://www.digit-eyes.com/gtin/v2_0/?upcCode=#{searched}&field_names=all&language=en&app_key=/+90u/IFDVg6&signature=KN/BBmEUE4tKFPemTydKvRdmADY=")
       @req = JSON.parse(@data.body_str)
-      # @manufacturer = @req['item']['manufacturer']
+
       @company = @req['manufacturer']['company']
-      #
+
       # require 'open-uri'
       # @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/#{searched}"))
       # @manufacturer = @doc.search('p')[1].text
     end
 
-    rescue StandardError
-    rescue Exception
+    # rescue StandardError
+    # rescue Exception
 
   end
 
