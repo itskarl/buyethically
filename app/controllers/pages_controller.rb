a12c343ec5284886111c52424c228fbfe11c8a25
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 01:03:34 -0400

    nokogiri

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
       searched = params[:upc_input]
       # @data = Curl::Easy.perform("http://api.walmartlabs.com/v1/items?apiKey=e7cvndf2d9pv8yv77cgnebcg&upc=#{searched}")
       # @req = JSON.parse(@data.body_str)
-      @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/012000172526"))
+      @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/#{searched}"))
       @manufacturer = @doc.search('p')[1].text
     end
 
