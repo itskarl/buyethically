8001d11639a4865e55457935713cf5ea80b66a13
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 01:23:11 -0400

    stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,11 @@ class PagesController < ApplicationController
       searched = params[:upc_input]
       # @data = Curl::Easy.perform("http://api.walmartlabs.com/v1/items?apiKey=e7cvndf2d9pv8yv77cgnebcg&upc=#{searched}")
       # @req = JSON.parse(@data.body_str)
+
+      require 'open-uri'
       @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/#{searched}"))
       @manufacturer = @doc.search('p')[1].text
     end
-
-
 
     rescue StandardError
     rescue Exception
