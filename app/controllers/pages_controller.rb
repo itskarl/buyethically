3d2708520fb9fc45e96e6db49b9c647b6fff16d0
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 12:15:11 -0400

    commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ class PagesController < ApplicationController
   def home
     if params[:upc_input]
       searched = params[:upc_input]
-      @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{search}")
+      @data = Curl::Easy.perform("	https://www.digit-eyes.com/gtin/v2_0/?upcCode=#{searched}&field_names=all&language=en&app_key=/+90u/IFDVg6&signature=KN/BBmEUE4tKFPemTydKvRdmADY=")
       @req = JSON.parse(@data.body_str)
-      @manufacturer = @req['item']['manufacturer']
-      @company = @req['item']['company_name']
+      # @manufacturer = @req['item']['manufacturer']
+      @company = @req['manufacturer']['company']
       #
       # require 'open-uri'
       # @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/#{searched}"))
