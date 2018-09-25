c605cd8a2f7d2bff2664d79aedf7befe3656e3fe
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-25 18:02:44 -0400

    test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,14 @@ class PagesController < ApplicationController
       searched = params[:upc_input]
       @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{searched}")
       @req = JSON.parse(@data.body_str)
+
+      #       require 'open-uri'
+      # @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/043695071238"))
+
     end
-  # 
-  # rescue StandardError
-  # rescue Exception
+
+    rescue StandardError
+    rescue Exception
   end
 
   def about
