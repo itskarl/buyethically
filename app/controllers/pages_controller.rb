bb7012deabcb152b4abf74b15c81fb529ed2ca75
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-26 10:17:36 -0400

    ah

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,14 +10,14 @@ class PagesController < ApplicationController
       @data2 = Curl::Easy.perform("https://bridge.buddyweb.fr/api/blorps/asdasdasdasd")
       @req2 = JSON.parse(@data2.body_str)
 
-
-      if @req['item']['company_name'][0..3].chars.count < 4
+      #creating an abbreviation from the company name to parse through the API
+      if @req['item']['company_name'][0..3].chars.count == 4
         @company_abbrev = @req['item']['company_name'][0..3].downcase
       else
         @company_abbrev = @req['item']['matched_items'][0]['manufacturer'][0..3].downcase
       end
 
-
+      #now comparing company found on UPC to list of companies in API
       @matched_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
 
     end
