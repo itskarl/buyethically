d49c3f23391e9c8668576101ed000b666ae7851d
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-26 01:02:43 -0400

    sometimes works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,13 +5,18 @@ class PagesController < ApplicationController
       @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{searched}")
       @req = JSON.parse(@data.body_str)
 
-      #       require 'open-uri'
+      # require 'open-uri'
       # @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/043695071238"))
+      @data2 = Curl::Easy.perform("https://bridge.buddyweb.fr/api/blorps/asdasdasdasd")
+      @req2 = JSON.parse(@data2.body_str)
+
+      @company_abbrev = @req['item']['company_name'][0..3].downcase
+      @matched_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
 
     end
 
-    rescue StandardError
-    rescue Exception
+    # rescue StandardError
+    # rescue Exception
   end
 
   def about
