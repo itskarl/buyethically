799e3e45b61be468cdde783cd215a0e81610d952
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-26 14:27:32 -0400

    ahh

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,20 +10,18 @@ class PagesController < ApplicationController
       @data2 = Curl::Easy.perform("https://bridge.buddyweb.fr/api/blorps/asdasdasdasd")
       @req2 = JSON.parse(@data2.body_str)
 
-      @thecompany = @req['item']['company_name']
-      @themanufacturer = @req['item']['matched_items'][0]['manufacturer']
+      @thecompany = @req['item']['company_name'].downcase.gsub("the ", "") if @req['item']['company_name']
+      @themanufacturer = @req['item']['matched_items'][0]['manufacturer'].downcase.gsub("the ", "") if @req['item']['matched_items'] != nil 
+
       #creating an abbreviation from the company name to parse through the API
       if @thecompany[0..3].chars.count == 4
         @company_abbrev = @thecompany[0..3].downcase
-        @matched_company = @req2.find {|x| x['company'].downcase.gsub("the ", "").match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
+        @matched_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
       end
       if @matched_company == nil
         @company_abbrev = @themanufacturer[0..3].downcase if @themanufacturer
-        @matched_company = @req2.find {|x| x['company'].downcase.gsub("the ", "").match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
+        @matched_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
       end
-
-      #now comparing company found on UPC to list of companies in API
-
 
     end
 
