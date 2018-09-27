1ccbc6feb80129b2b363546a5b6161da2c5c1b0a
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-26 23:57:25 -0400

    ee

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
+
+    #the search begins here!!
     if params[:upc_input]
       searched = params[:upc_input]
       @data = Curl::Easy.perform("https://api.barcodable.com/api/v1/upc/#{searched}")
@@ -8,11 +10,14 @@ class PagesController < ApplicationController
       # require 'open-uri'
       # @doc = Nokogiri::HTML(open("https://www.barcodable.com/upc/043695071238"))
 
-      # this is my own API, where we find company info
-      @data2 = Curl::Easy.perform("https://bridge.buddyweb.fr/api/blorps/asdasdasdasd")
+      # this is my own API, uploaded to buddyweb server where we find company info
+      @data2 = Curl::Easy.perform("https://bridge.buddyweb.fr/api/companyrating/companyratings")
       @req2 = JSON.parse(@data2.body_str)
 
-      @thecompany = @req['item']['company_name'].downcase.gsub("the ", "") if @req['item']['company_name']
+      if @req['item']['company_name']
+      @thecompany = @req['item']['company_name'].downcase.gsub("the ", "")
+      end
+
       if @req['item']['matched_items'][0]
         @themanufacturer = @req['item']['matched_items'][0]['manufacturer'].downcase.gsub("the ", "")
       else
@@ -22,11 +27,32 @@ class PagesController < ApplicationController
       #creating an abbreviation from the company name to parse through the API
       if @thecompany[0..3].chars.count == 4
         @company_abbrev = @thecompany[0..3].downcase
-        @matched_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
+        @found_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)} if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
+        @matched_company = @found_company['company'] if @found_company
       end
+
+      #if you cant match the company name, try the manufacturer name
       if @matched_company == nil
         @company_abbrev = @themanufacturer[0..3].downcase if @themanufacturer
-        @matched_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}['company'] if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
+        @found_company = @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)} if @req2.find {|x| x['company'].downcase.match(/#{@company_abbrev}/)}
+        @matched_company = @found_company['company'] if @found_company
+      end
+
+
+      if @found_company && @matched_company
+        @company_score = @found_company['score']
+
+        #newsAPI here!
+        @newssearchterm = @matched_company.gsub(/[^0-9A-Za-z]/, ' ').gsub!(/\s/,'+')
+        @daterange = (Time.now - 28.days).strftime("%C%y-%m-%d")
+
+        @newsdata = Curl::Easy.perform("https://newsapi.org/v2/everything?q=#{@newssearchterm}&from=#{@daterange}&sortBy=popularity&apiKey=bcd7a32090a74b88b8730f2c5540a792")
+        p @matched_company
+        p @daterange
+        @newsreq = JSON.parse(@newsdata.body_str)
+
+        @newsarticles = @newsreq['articles']
+        #newsAPI here!
       end
 
     end
